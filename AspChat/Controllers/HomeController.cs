c851a771f9233a545113305ea6e33260bfd4cc77
maksimn <maksim1989sovunion@yandex.ru>
2016-10-20 00:05:22 +0300

    Init

---
+++ AspChat/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Mvc;
+
+namespace AspChat.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            return View();
+        }
+	}
+}
