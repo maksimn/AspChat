fa85b671f34470207a1dc0d14b251e10cb32e307
maksimn <maksim1989sovunion@yandex.ru>
2016-10-20 15:02:14 +0300

    Рабочий пример простого чата с habr

---
+++ AspChat/ChatHandler.ashx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.WebSockets;
+
+namespace AspChat {
+    public class ChatHandler : IHttpHandler {
+        // Список всех клиентов
+        private static readonly List<WebSocket> Clients = new List<WebSocket>();
+
+        // Блокировка для обеспечения потокобезопасности
+        private static readonly ReaderWriterLockSlim Locker = new ReaderWriterLockSlim();
+
+        public bool IsReusable {
+            get { return true; }
+        }
+
+        public void ProcessRequest(HttpContext context) {
+            if (context.IsWebSocketRequest) {
+                context.AcceptWebSocketRequest(WebSocketRequest);
+            }
+        }
+
+        private async Task WebSocketRequest(AspNetWebSocketContext context) {
+            // Получаем сокет клиента из контекста запроса
+            var socket = context.WebSocket;
+
+            // Добавляем его в список клиентов
+            Locker.EnterWriteLock();
+            try {
+                Clients.Add(socket);
+            } finally {
+                Locker.ExitWriteLock();
+            }
+
+            // Слушаем его
+            while (true) {
+                var buffer = new ArraySegment<byte>(new byte[1024]);
+
+                // Ожидаем данные от него
+                var result = await socket.ReceiveAsync(buffer, CancellationToken.None);
+
+
+                //Передаём сообщение всем клиентам
+                for (int i = 0; i < Clients.Count; i++) {
+
+                    WebSocket client = Clients[i];
+
+                    try {
+                        if (client.State == WebSocketState.Open) {
+                            await client.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
+                        }
+                    } catch (ObjectDisposedException) {
+                        Locker.EnterWriteLock();
+                        try {
+                            Clients.Remove(client);
+                            i--;
+                        } finally {
+                            Locker.ExitWriteLock();
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
