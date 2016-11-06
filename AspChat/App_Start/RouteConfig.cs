a8831a479d251fea36d1e376f4cec4d7ac11e26b
maksimn <maksim1989sovunion@yandex.ru>
2016-11-06 06:08:22 +0300

    Замена дефолтного обработчика маршрутов рукописным

--- AspChat/App_Start/RouteConfig.cs
+++ AspChat/App_Start/RouteConfig.cs
@@ -1,23 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace AspChat
-{
-    public class RouteConfig
-    {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
+namespace AspChat {
+    public class RouteConfig {
+        public static void RegisterRoutes(RouteCollection routes) {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.Add("Empty", new Route(url: "", routeHandler: new ChatRouteHandler()));
+        }
+    }
+
+    class ChatRouteHandler : IRouteHandler {
+        public IHttpHandler GetHttpHandler(RequestContext requestContext) {
+            var url = requestContext.HttpContext.Request.Url != null ? 
+                      requestContext.HttpContext.Request.Url : null;
+            if (url != null && url.AbsolutePath.Length == 1) {
+                requestContext.RouteData.Values["controller"] = "home";
+                requestContext.RouteData.Values["action"] = "index";                
+            }
+            return new MvcHandler(requestContext);
         }
     }
 }
