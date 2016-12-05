9c6bc96986cd11788f9a93c71578b3ef59f012b3
maksimn <maksim1989sovunion@yandex.ru>
2016-12-05 09:16:58 +0300

    Заголовок страницы

--- AspChat/Client/js/components/Layout/Layout.js
+++ AspChat/Client/js/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { connect } from "react-redux"
 
-import ChatMessageInputForm from "./ChatMessageInputForm";
-import ChatMessagesListView from "./ChatMessagesListView";
+import ChatMessageInputForm from "../ChatMessageInputForm";
+import ChatMessagesListView from "../ChatMessagesListView";
 
-import socket from "../webSocket";
+import socket from "../../webSocket";
+import styles from "./Layout.css";
 
 export class Layout extends React.Component {
     sendChatMessage(msgObj) {
@@ -20,6 +21,17 @@ export class Layout extends React.Component {
         const chatUserName = this.props.userName;
         return (
             <div>
+                <header>
+                    <div className={ styles.userSessionArea }>
+                        <div className={ styles.userName }>
+                            {chatUserName}
+                        </div>
+                        <div className={ styles.userMenuArrowWrapper }>
+                            <div className={ styles.userMenuArrow }>
+                            </div>
+                        </div>  
+                    </div>
+                </header>
                 <ChatMessageInputForm userName={chatUserName} 
                                       addChatMessage={this.sendChatMessage.bind(this)} />
                 <ChatMessagesListView userName={chatUserName}
