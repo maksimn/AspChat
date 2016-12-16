24693f3c31a1024e45bdfb3e29a9522ce3f5d9e9
maksimn <maksim1989sovunion@yandex.ru>
2016-12-16 12:38:47 +0300

    Форма регистрации пользователя

--- AspChat/Client/js/pages/RegisterPage/RegisterPage.js
+++ AspChat/Client/js/pages/RegisterPage/RegisterPage.js
@@ -7,7 +7,30 @@ export default class RegisterPage extends React.Component {
     render() {
         return (
             <div>
-                <Link to="/">Назад</Link>
+                <div className={ styles.backLinkWrap }>
+                    <Link className={ styles.backLink } to="/">Назад</Link>
+                </div>
+                <div className={ styles.registerArea }>
+                    <form
+                          className={ styles.registerForm } method="post" action="register">
+                        <div className={ styles.registerFormHeader }>Регистрация в чате</div>
+                        <div className={ styles.formField }>
+                            <label>Имя:</label><br />
+                            <input type="text" required />
+                        </div>
+                        <div className={ styles.formField }>
+                            <label>Пароль:</label><br />
+                            <input id="password" type="password" required />                     
+                        </div>
+                        <div className={ styles.formField }>
+                            <label>Повторите пароль:</label><br />
+                            <input id="password2" type="password" required />
+                        </div>
+                        <div className={ styles.formSubmit }>
+                            <input type="submit" value="Отправить" />
+                        </div>
+                    </form>
+                </div>
             </div>
         );
     }
