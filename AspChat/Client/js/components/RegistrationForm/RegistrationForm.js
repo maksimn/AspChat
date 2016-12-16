98f4a7548253447fab82eb24acb0b61df2a8347a
maksimn <maksim1989sovunion@yandex.ru>
2016-12-16 15:00:56 +0300

    Рефакторинг: форма регистрации сделана отдельным компонентом

---
+++ AspChat/Client/js/components/RegistrationForm/RegistrationForm.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+import styles from "./RegistrationForm.css";
+
+export default class RegistrationForm extends React.Component {
+    isFormDataValid() {
+        const psw = document.getElementById("password").value;
+        const psw1 = document.getElementById("password1").value;
+        if (psw && psw1 && (psw !== psw1)) {
+            return false;
+        }
+        return true;
+    }
+
+    clientValidation(e) {
+        if (!this.isFormDataValid()) {
+            e.preventDefault();
+            this.showValidationErrors();
+        }
+    }
+
+    showValidationErrors() {
+        let errorsArea = document.querySelector(`.${styles.formInputErrors }`);
+        errorsArea.innerHTML = "Введенные значения пароля должны совпадать";
+    }
+    render() {
+        return(
+            <form className={styles.registerForm} method="post" action="register">
+                <div className={styles.registerFormHeader}>Регистрация в чате</div>
+                <div className={styles.formField}>
+                    <label>Имя:</label><br />
+                    <input type="text" required />
+                </div>
+                <div className={styles.formField}>
+                    <label>Пароль:</label><br />
+                    <input id="password" type="password" required />
+                </div>
+                <div className={styles.formField}>
+                    <label>Повторите пароль:</label><br />
+                    <input id="password1" type="password" required />
+                </div>
+                <div className={styles.formSubmit}>
+                    <input type="submit" value="Отправить"
+                        onClick={this.clientValidation.bind(this)}
+                    />
+                </div>
+                <div className={styles.formInputErrors}></div>
+            </form>
+        );
+    }
+}
--- AspChat/Client/js/pages/RegisterPage/RegisterPage.js
+++ AspChat/Client/js/pages/RegisterPage/RegisterPage.js
@@ -2,58 +2,17 @@ import React from "react";
 import { Link } from "react-router";
 
 import styles from "./RegisterPage.css";
+import RegistrationForm from "../../components/RegistrationForm";
 
 export default class RegisterPage extends React.Component {
-    isFormDataValid() {
-        const psw = document.getElementById("password").value;
-        const psw1 = document.getElementById("password1").value;
-        if (psw !== psw1) {
-            return false;
-        }
-        return true;
-    }
-
-    clientValidation(e) {
-        if (!this.isFormDataValid()) {
-            e.preventDefault();
-        }
-        this.showValidationErrors();
-    }
-
-    showValidationErrors() {
-        let errorsArea = document.querySelector(`.${styles.formInputErrors }`);
-        errorsArea.innerHTML = "Введенные значения пароля должны совпадать";
-    }
-
     render() {
         return (
             <div>
                 <div className={ styles.backLinkWrap }>
-                    <Link className={ styles.backLink } to="/">Назад</Link>
+                    <Link to="/">Назад</Link>
                 </div>
                 <div className={ styles.registerArea }>
-                    <form
-                          className={ styles.registerForm } method="post" action="register">
-                        <div className={ styles.registerFormHeader }>Регистрация в чате</div>
-                        <div className={ styles.formField }>
-                            <label>Имя:</label><br />
-                            <input type="text" required />
-                        </div>
-                        <div className={ styles.formField }>
-                            <label>Пароль:</label><br />
-                            <input id="password" type="password" required />                     
-                        </div>
-                        <div className={ styles.formField }>
-                            <label>Повторите пароль:</label><br />
-                            <input id="password1" type="password" required />
-                        </div>
-                        <div className={ styles.formSubmit }>
-                            <input type="submit" value="Отправить" 
-                                onClick={ this.clientValidation.bind(this) }
-                            />
-                        </div>
-                        <div className={ styles.formInputErrors }></div>
-                    </form>
+                    <RegistrationForm />
                 </div>
             </div>
         );
