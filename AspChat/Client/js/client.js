933ab02abe2eb8e77defcfa46e6f301b3934bf00
maksimn <maksim1989sovunion@yandex.ru>
2016-12-17 21:10:09 +0300

    Авторизация: можно зайти в комнату чата, но сокеты не работают

--- AspChat/Client/js/client.js
+++ AspChat/Client/js/client.js
@@ -8,20 +8,28 @@ import App from "./pages/App";
 import StartPage from "./pages/StartPage";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
-
+import ChatRoom from "./pages/ChatRoom";
 import store from './store';
 
 const history = syncHistoryWithStore(hashHistory, store);
 
 const app = document.getElementById('app');
+
+let routes = [];
+
+if (window.isAuth) {
+    routes.push(<IndexRoute key={0} component={ChatRoom}></IndexRoute>);
+} else {
+    routes.push(<IndexRoute key={1} component={StartPage}></IndexRoute>);
+    routes.push(<Route key={2} path="register" component={RegisterPage}></Route>);
+    routes.push(<Route key={3} path="login" component={LoginPage}></Route>);
+}
+
 ReactDOM.render(
     <Provider store={store}>
         <Router history={history}>
             <Route path="/" component={App}>
-                <IndexRoute component={StartPage}></IndexRoute>
-                <Route path="register" component={RegisterPage}></Route>
-                <Route path="login" component={LoginPage}></Route>
-
+                {routes}
             </Route>
         </Router>
     </Provider>
