1b97e312b7a2653eae729daa116db73ce05f7db7
maksimn <maksim1989sovunion@yandex.ru>
2016-12-18 12:20:37 +0300

    Удаление лишнего кода

--- AspChat/Client/js/pages/ChatRoom/ChatRoom.js
+++ AspChat/Client/js/pages/ChatRoom/ChatRoom.js
@@ -1,21 +1,9 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Link } from "react-router";
 
 import ChatMessageInputForm from "../../components/ChatMessageInputForm";
 import ChatMessagesListView from "../../components/ChatMessagesListView";
 import styles from "./ChatRoom.css";
-
-function deleteAllCookies() {
-    var cookies = document.cookie.split(";");
-
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i];
-        var eqPos = cookie.indexOf("=");
-        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    }
-}
 
 export class ChatRoom extends React.Component {
     render() {
@@ -26,8 +14,7 @@ export class ChatRoom extends React.Component {
                     <div className={ styles.userName }>
                         {chatUserName}
                     </div>
-                    <form onSubmit={deleteAllCookies} 
-                          className={ styles.logOutForm } 
+                    <form className={ styles.logOutForm } 
                           method="POST" 
                           action="logout">
                         <input type="submit" value="Выйти" />
